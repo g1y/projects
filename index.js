799b5e2a04aaf28d0bdaa4c99242df3b8d158284
Guy Moore <guy.moore.normal@gmail.com>
2018-01-06 16:21:35 -0800

    Move ProjectUnit component to a separate dir
    This directory is the default location used by styleguideist

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-var ProjectUnit = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<h4>Project</h4>
-				<div><p>This project does some stuff.</p></div>
-			</div>
-		);
-	}
-});
-
 React.render(
 	<div>Bonjour!</div>,
 	<ProjectUnit />,
---
+++ src/components/ProjectUnit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ProjectUnit extends React.Component  {
+	render() {
+		return (
+			<div>
+				<h4>Hello</h4>
+				<div><p>This project does some stuff.</p></div>
+			</div>
+		);
+	}
+}
