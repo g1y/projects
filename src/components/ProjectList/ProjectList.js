861b2531d3a38a3e7624bbd17792c689ecce41fd
Guy Moore <guy.moore.normal@gmail.com>
2018-01-07 23:43:13 -0800

    Set skeleton request structure for graphQL

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -17,4 +17,20 @@ export default class ProjectList extends React.Component  {
 			</div>
 		);
 	}
+
+	getData() {
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
+		xhr.open("POST", "/graphql");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.onload = function () {
+			  console.log('data returned:', xhr.response);
+		}
+		var query = `query  {}`;
+		xhr.send(JSON.stringify({
+			  query: query,
+			  variables: {},
+		}));
+	}
 }
